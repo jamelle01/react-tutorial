2a9a9b7602542fa58fe4bce7114b6c5e547f7866
Ell do <91648854+jamelle01@users.noreply.github.com>
2022-10-25 01:15:53 +0800

    add right redirection

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -15,7 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={ <Home/> } />
             <Route path="/create" element={ <Create/> } />
-            <Route path="/blogs/:id/*" element={ <BlogDetails/> } />
+            <Route path="/blogs/:id" element={ <BlogDetails/> } />
           </Routes>
         </div>
       </div>
